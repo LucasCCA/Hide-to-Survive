ead79294e4e88e053f6b30a55f377e90dfd58948
Lucas <lucasamarale@hotmail.com>
2022-08-06 19:26:13 -0300

    Added Player Movement

--- ZombieSurvival/Assets/Scripts/PlayerMovement.cs
+++ ZombieSurvival/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3f;
+
+    Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+
+        if(!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+
+        if(Input.GetButton("Horizontal"))
+        {
+            animator.SetBool("Walking", true);
+        }
+        else if(Input.GetButtonUp("Horizontal"))
+        {
+            animator.SetBool("Walking", false);
+        }
+        
     }
 }
