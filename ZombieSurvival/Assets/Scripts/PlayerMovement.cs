7673064dcd4c4f0c30d79f106c763cf8345349c7
Lucas <lucasamarale@hotmail.com>
2022-08-07 01:03:59 -0300

    Cleaned Player Movement Script

--- ZombieSurvival/Assets/Scripts/PlayerMovement.cs
+++ ZombieSurvival/Assets/Scripts/PlayerMovement.cs
@@ -15,20 +15,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        Walk();
+    }
+
+    void Walk()
+    {
         float movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
 
-        if(!Mathf.Approximately(0, movement))
+        if (!Mathf.Approximately(0, movement))
+        {
             transform.rotation = movement > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+        }
 
-        if(Input.GetButton("Horizontal"))
+        if (Input.GetButton("Horizontal"))
         {
             animator.SetBool("Walking", true);
         }
-        else if(Input.GetButtonUp("Horizontal"))
+        else if (Input.GetButtonUp("Horizontal"))
         {
             animator.SetBool("Walking", false);
         }
-        
     }
 }
