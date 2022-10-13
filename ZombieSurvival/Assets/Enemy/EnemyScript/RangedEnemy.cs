1b70ebaa0ab0c7e2dc53eb9fa53151d3ac28a97b
Lucas <lucasamarale@hotmail.com>
2022-10-13 19:31:51 -0300

    Inicio Segundo Inimigo

---
+++ ZombieSurvival/Assets/Enemy/EnemyScript/RangedEnemy.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Ranged ")]
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireBalls;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    //References
+    private Animator anim;
+    private EnemyPatrol enemyPatrol;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if(PlayerInSight())
+        {
+            if(cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("RangedAttack");
+            }
+        }
+
+        if(enemyPatrol != null)
+        {
+            enemyPatrol.enabled = !PlayerInSight();
+        }
+    }
+
+    private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        //Shoot projectile
+        fireBalls[FindFireBall()].transform.position = firePoint.position;
+        fireBalls[FindFireBall()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+
+    private int FindFireBall()
+    {
+        for(int i = 0; i < fireBalls.Length; i++)
+        {
+            if (!fireBalls[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    private bool PlayerInSight()
+    {
+        Vector2 hitOrigin = boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 hitSize = new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z);
+        RaycastHit2D hit = Physics2D.BoxCast(hitOrigin, hitSize, 0, Vector2.left, 0, playerLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector2 hitDrawCenter = boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 hitDrawSize = new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(hitDrawCenter, hitDrawSize);
+    }
+}
