c870647536bba4c735731d150a6f3a1c6f096633
Júlia Vasconcelos <juliavasconceloscr@gmail.com>
2022-10-31 12:21:28 -0300

    Inicio de um novo inimigo

---
+++ ZombieSurvival/Assets/Enemy/EnemyScript/EnemyAILobo.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAILobo : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private Animator animator;
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private LoboPatrol enemyPatrol;
+
+
+    private bool ativaAtaque = false;
+    public float range;
+
+    private void Update()
+    {
+        SetAttack();
+    }
+
+    private bool PlayerinSight()
+    {
+        Vector2 hitOrigin = boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 hitSize = new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z);
+        RaycastHit2D hit = Physics2D.BoxCast(hitOrigin, hitSize, 0, Vector2.left, 0, playerLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector2 hitDrawCenter = boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 hitDrawSize = new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(hitDrawCenter, hitDrawSize);
+    }
+    private void SetAttack()
+    {
+        if (PlayerinSight())
+        {
+            ativaAtaque = true;
+            if (ativaAtaque)
+            {
+                animator.SetBool("andar", false);
+                enemyPatrol.enabled = false;
+                animator.SetBool("loboBatendo", true);
+            }
+
+        }
+        else
+        {
+            animator.SetBool("loboBatendo", false);
+            animator.SetBool("andar", true);
+            enemyPatrol.enabled = true;
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EnemyKillPlayer();
+        }
+    }
+    private float EnemyKillPlayer()
+    {
+        return PlayerHealth.playerCurrentHealth -= damage;
+    }
+}
---
+++ ZombieSurvival/Assets/Enemy/EnemyScript/LoboPatrol.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoboPatrol : MonoBehaviour
+{
+    [Header("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Move parameters")]
+    public float enemySpeed;
+    [SerializeField] private EnemyAILobo enemyAI;
+
+    [SerializeField] Animator anim;
+    private Vector3 initScale;
+    private bool movingLeft;
+    void Awake()
+    {
+        anim.SetBool("andar", true);
+        initScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (movingLeft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+            {
+                UpdateCourseLeft();
+            }
+            else
+            {
+                DirectionChange();
+
+            }
+
+        }
+        else
+        {
+            if (enemy.position.x <= rightEdge.position.x)
+            {
+                UpdateCourseRight();
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+
+    }
+
+    public void DirectionChange()
+    {
+        movingLeft = !movingLeft;
+    }
+
+    public void MoveinDirection(int direction)
+    {
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * direction, initScale.y, initScale.z);
+        float axisX = enemy.position.x + Time.deltaTime * direction * enemySpeed;
+        enemy.position = new Vector3(axisX, enemy.position.y, enemy.position.z);
+    }
+
+    private void UpdateRangeWhenInLeft()
+    {
+        enemyAI.range = Mathf.Abs(enemy.position.x - leftEdge.position.x) / 3f;
+    }
+
+    private void UpdateRangeWhenInRight()
+    {
+        enemyAI.range = Mathf.Abs(rightEdge.position.x - enemy.position.x) / 2.8f;
+    }
+
+    public void UpdateCourseLeft()
+    {
+        MoveinDirection(-1);
+        UpdateRangeWhenInLeft();
+    }
+
+    public void UpdateCourseRight()
+    {
+        MoveinDirection(1);
+        UpdateRangeWhenInRight();
+    }
+}
